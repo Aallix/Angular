83432334de301f02b44d2b1a81a54453dadfe97a
Aliaksandr Stasilevich <aliaksandr.stasilevich@xplanis.com>
2025-02-20 22:38:44 +0100

    Homework2

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//import { RouterOutlet } from '@angular/router';
+import { UserInfoComponent } from './user-info/user-info.component';
+import { MoreInfoComponent } from './more-info/more-info.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [UserInfoComponent, MoreInfoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
